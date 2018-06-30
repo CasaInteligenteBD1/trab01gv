abfe2f8314e0026d10199bba3e638a47e159ecc7
Andreangelo <36939704+andreangelopp@users.noreply.github.com>
2018-06-29 22:53:34 -0300

    Correção do 3º select (fk_estado_id = 8)

--- arquivos/9.2.sql
+++ arquivos/9.2.sql
@@ -1,5 +1,5 @@
 SELECT id, numero, andar, status, fk_estacionamento_id FROM vaga WHERE status = 'ocupada';
 SELECT id, nome AS Estacionamento, valorhora FROM estacionamento WHERE CAST(valorhora AS numeric) <= 10;
-SELECT nome AS Cidade from cidade WHERE fk_estado_id = 1;
+SELECT nome AS Cidade from cidade WHERE fk_estado_id = 8;
 SELECT * FROM funcionario WHERE fk_cargo_id = 1;
 SELECT * FROM reserva WHERE datareserva = '2018-04-22';
